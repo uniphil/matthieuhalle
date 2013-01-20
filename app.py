edb6028e31481d653baf0e9873ad0ebedff1d4e4
Philip Schleihauf <uniphil@gmail.com>
2013-01-19 20:29:50 -0500

    style and add photos

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python
 
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, Response
 from yaml import load as yamload
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def matthieuhalle():
     # nothing here, go to Margraue
     return redirect(url_for('margraue'))
 
+
 @app.route('/margraue')
 def margraue():
     context = yamload(open('margraue.yaml'))
     return render_template('margraue.html', **context)
+
+
+@app.route('/humans.txt')
+def humans():
+    with open('humans.txt') as humanstxt:
+        return Response(humanstxt.read(), mimetype='text/plain')
 
 
 if __name__ == '__main__':
