a5d876fd2a59466f804784a314bc392ecfbf5892
Philip Schleihauf <uniphil@gmail.com>
2013-01-22 10:38:35 -0500

    interactive thumbnail hovers

--- static/main.js
+++ static/main.js
@@ -1,1 +1,14 @@
-$('html').removeClass('no-js').addClass('js');
+$(document).ready(function() {
+
+    $('html').removeClass('no-js').addClass('js');
+
+    $('#photos img').hover(function() {
+        var full = $(this).parent().attr('href');
+        $('#photo-overlay')
+            .css('background-image', 'url("' + full + '"")')
+            .addClass('visible');
+    }, function() {
+        $('#photo-overlay').removeClass('visible');
+    });
+
+});
